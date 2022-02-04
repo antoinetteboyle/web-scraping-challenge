3dafc8547dc160898de6d1bafaa20386b0d9999e
antoinetteboyle <antoinetteboyle75@gmail.com>
2022-02-05 09:39:57 +1100

    Progress in code app.py index.html and scrape_mars

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,51 +1,35 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_costa
+import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
-
-#INSTRUCTIONS: * In `app.py`, complete the `/scrape` route to store
-#  the Python dictionary as a document in a mongo database collection.
-#* In `app.py`, complete the `/` route to read one entry from mongo 
-#  and render the flask template with the mongo data.
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
-    # PREVIOUS EXAMPLE:
-    #listings = mongo.db.listings.find_one()
-    #return render_template("index.html", listings=listings)
-
+    
     # Find one record of data from the mongo database
-    # @TODO: YOUR CODE HERE!
-    destination_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", mars=mars_data)
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-    # PREVIOUS EXAMPLE:
-    # listings = mongo.db.listings
-    # listings_data = scrape_costa.scrape()
-    # listings.update({}, listings_data, upsert=True)
-    # return redirect("/", code=302)
 
     # Run the scrape function and save the results to a variable
-    # @TODO: YOUR CODE HERE!
-    costa_data = scrape_costa.scrape_info()
+    mars_data = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    # @TODO: YOUR CODE HERE!
-    mongo.db.collection.update_one({}, {"$set": costa_data}, upsert=True)
+    mongo.db.collection.update_one({}, {"$set": mars_data}, upsert=True)
+   
     # Redirect back to home page
-    return redirect("/")
-
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,93 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
+import pandas as pd
+
+def scrape_info():
+    # Set up Requests
+    url = 'https://redplanetscience.com/'
+    response = requests.get(url)
+    soup = bs(response.text, 'html.parser')
+    news_title = soup.title.text.strip()
+    news_paragraph = soup.body.p.text.strip()
+
+    # Set up Splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    # Visit website and scrape page to soup
+    url_image = 'https://spaceimages-mars.com'
+    browser.visit(url_image)
+    time.sleep(1)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    # Find the featured image
+    featured_image_url = soup.find_all('img')[1]['src']
+    featured_image_url = url_image + "/" + featured_image_url  
+
+    #MARS FACTS:
+    #Use Pandas to scrape the table
+    url = 'https://galaxyfacts-mars.com'
+    facts_mars = pd.read_html(url,header=0)
+    facts_mars = pd.concat(facts_mars)
+    del facts_mars['Equatorial Diameter:']
+    del facts_mars['6,792 km']
+    facts_mars.drop_duplicates(subset=None, keep=False, inplace=True, ignore_index=False)
+    df = pd.DataFrame(facts_mars)
+    df1 = df.reset_index(drop=True)
+    tables = df1.set_index("Mars - Earth Comparison")
+
+
+    # Obtain high resolution images for each of Mar's hemispheres. 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    #Find mars hemisphere URLs
+    url_list = []
+    for x in range(4,12,1):
+     link = soup.find_all('a')[x]['href']
+     url = 'https://marshemispheres.com/'
+     link_url = url + link
+     response = requests.get(link_url)
+     soup = bs(response.text, 'lxml')
+     #find all images
+     imgs = soup.find_all('img', class_= "wide-image")
+     for img in imgs:  
+        url_list.append(url + img['src'])
+    print(url_list)
+    #Find mars hemisphere titles
+    title_list = []
+    for x in range(4,12,1):
+      link = soup.find_all('a')[x]['href']
+      url = 'https://marshemispheres.com/'
+      link_url = url + link
+      response = requests.get(link_url)
+      soup = bs(response.text, 'lxml')
+      titles = soup.find_all('h2', class_= "title")
+      for title in titles:
+        title_list.append(title.text.strip())
+    #Dictionary mars hemispheres:
+    hemisphere_image_urls = [
+    {"title": title_list[0], "img_url": url_list[0]},
+    {"title": title_list[1], "img_url": url_list[1]},
+    {"title": title_list[2], "img_url": url_list[2]},
+    {"title": title_list[3], "img_url": url_list[3]},
+    ]
+
+    # Quite the browser after scraping
+    browser.quit()
+
+  #Return one Python dictionary containing all of the scraped data.
+    mars_data = {
+    "news_title":news_title,
+    "news_paragraph":news_paragraph,
+    "featured_image_url":featured_image_url,
+    "tables":tables,
+    "hemisphere_image_urls":hemisphere_image_urls
+    }
+    # Return results
+    return mars_data
+    
